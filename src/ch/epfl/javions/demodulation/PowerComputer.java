b5ec5d76bbd361c4b0c711b310c4018df2691944
Sensiel <sennesiel2@gmail.com>
2023-03-05 21:09:08 +0100

    SampleDecoder et PowerComputer doivent être verif

--- src/ch/epfl/javions/demodulation/PowerComputer.java
+++ src/ch/epfl/javions/demodulation/PowerComputer.java
@@ -15,7 +15,7 @@ public final class PowerComputer {
 
     public PowerComputer(InputStream stream, int batchSize){
         Preconditions.checkArgument(batchSize % 8 == 0 && batchSize >= 0);
-        samplesDecoder = new SamplesDecoder(stream, batchSize);
+        samplesDecoder = new SamplesDecoder(stream, batchSize * 2);
         batch = new short[batchSize];
         this.batchSize = batchSize;
     }
@@ -24,9 +24,14 @@ public final class PowerComputer {
         Preconditions.checkArgument(batch.length == batchSize);
         int nSampleRead = samplesDecoder.readBatch(this.batch);
         currSample = new short[8];
-        for(int iSample = 0; iSample < batchSize/2; iSample++){
-            currSample[(iSample*2)%8] = this.batch[iSample*2];
-            currSample[(iSample*2 + 1)%8] = this.batch[iSample*2 + 1];
+        for(int iPowerSample = 0; iPowerSample < batchSize; iPowerSample++){
+            int iSample = iPowerSample*2;
+            currSample[iSample] = this.batch[iSample];
+            currSample[(iSample + 1)%8] = this.batch[iSample + 1];
+            if(iPowerSample >= 4){ // The following formula is P(n) but we added 8 to each index used in the formula ( because (iSample - 6)%8 == (iSample + 2)%8 and it's easier to calculate)
+                batch[iPowerSample] = (int)Math.pow(currSample[(iSample + 2) % 8] - currSample[(iSample + 4) % 8] + currSample[(iSample + 6) % 8] - currSample[iSample % 8], 2)
+                +  (int)Math.pow(currSample[(iSample + 1) % 8] - currSample[(iSample + 3) % 8] + currSample[(iSample + 5) % 8] - currSample[(iSample + 7) % 8], 2);
+            }
         }
 
         return nSampleRead;
