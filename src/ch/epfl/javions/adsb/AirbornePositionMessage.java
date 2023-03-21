d9401b8620bc30876daf6f13ab69dd82bf139185
Sensiel <sennesiel2@gmail.com>
2023-03-22 00:52:02 +0100

    AirbornePositionMessage est devenu lisible wtf

--- src/ch/epfl/javions/adsb/AirbornePositionMessage.java
+++ src/ch/epfl/javions/adsb/AirbornePositionMessage.java
@@ -26,7 +26,7 @@ public record AirbornePositionMessage(long timeStampNs,
         Objects.requireNonNull(icaoAddress);
         Preconditions.checkArgument(timeStampNs >= 0);
         Preconditions.checkArgument(parity == 1 || parity == 0);
-        Preconditions.checkArgument((x >= 0 && x < 1) && (y >= 0 && y < 1));
+        Preconditions.checkArgument((x >= 0d && x < 1d) && (y >= 0d && y < 1d));
     }
 
     /**
@@ -35,43 +35,42 @@ public record AirbornePositionMessage(long timeStampNs,
      */
     public static AirbornePositionMessage of(RawMessage rawMessage){
         long ME = rawMessage.payload();
-        long timeStampsNs = rawMessage.timeStampNs();
-        int parity = Bits.extractUInt(ME,34,1);
-        IcaoAddress icaoAddress = rawMessage.icaoAddress();
-        double latLocal = Math.scalb(Bits.extractUInt(ME,17,17),-17);
-        double lonLocal = Math.scalb(Bits.extractUInt(ME,0,17),-17);
         long ALT = Bits.extractUInt(ME,36,12);
-        // décodage ALT
+
         double altitudeFinale;
-        //degueu
-        byte grpD = 0;
-        byte grpA = 0;
-        byte grpB = 0;
-        byte grpC = 0;
-        if(Bits.testBit(ME,4)){
+
+        if(Bits.testBit(ALT,4)){
             int part1 = Bits.extractUInt(ME,5,7);
             int part2 = Bits.extractUInt(ME,0,4);
-            altitudeFinale = part1 << 4 | part2;
-        } else {
-            // démelage
-            for(int i = 0; i <= 2; ++i){
-                grpD = (byte)(grpD << 1 | Bits.extractUInt(ALT,4-2*i,1));
-                grpA = (byte)(grpA << 1 | Bits.extractUInt(ALT,10-2*i,1));
-                grpB = (byte)(grpB << 1 | Bits.extractUInt(ALT,5-2*i,1));
-                grpC = (byte)(grpC << 1 | Bits.extractUInt(ALT,11-2*i,1));
-            }
-            long altitude = (( ((grpD << 3) | grpA) << 3) | grpB) << 3 | grpC ; // c hyper moche
-            // conversion en gray
-            byte pdsfaible = (byte) Bits.extractUInt(altitude,0,3);
-            byte pdsfort = (byte) Bits.extractUInt(altitude,3,9);
-            int pdsFaibleGray = (pdsfaible >> 0)^(pdsfaible >> 1)^(pdsfaible >> 2);
-            int pdsFortGray = 0;
-            for(int i = 0 ; i < 9; i++){ pdsFortGray ^=  pdsfort >> i; }
-            if(pdsFaibleGray == 0 || pdsFaibleGray == 5 || pdsFaibleGray == 6){ return null; }
-            if(pdsFaibleGray == 7){ pdsFaibleGray = 5; }
-            if(pdsFortGray % 2 == 1){ pdsFaibleGray = 6 - pdsFaibleGray; }
-            altitudeFinale = Units.convertFrom(-1300 + (pdsFaibleGray * 100) + (pdsFortGray * 500),Units.Length.FOOT);
+            altitudeFinale = (part1 << 4 | part2)*25d - 1000;
         }
+        else {
+            long altitude = 0L;
+            int[] index = {9,3,10,4,11,5,6,0,7,1,8,2};
+            for(int iBit = 0; iBit < 12; iBit++){
+                long currBit = Bits.testBit(ALT, iBit) ? 1L : 0L;
+                altitude = altitude | (currBit << index[iBit]);
+            }
+
+            int lowPart = Bits.extractUInt(altitude,0,3);
+            int upPart = Bits.extractUInt(altitude,3,9);
+            int lowGray = (lowPart)^(lowPart >> 1)^(lowPart >> 2);
+            int upGray = 0;
+            for(int i = 0 ; i < 9; i++)
+                upGray ^= upPart >> i;
+
+            if (lowGray == 0 || lowGray == 5 || lowGray == 6) return null;
+            if (lowGray == 7) lowGray = 5;
+            if(upGray % 2 == 1) lowGray = 6 - lowGray;
+
+            altitudeFinale = Units.convertFrom(-1300 + (lowGray * 100) + (upGray * 500),Units.Length.FOOT);
+        }
+
+        double lonLocal = Math.scalb(Bits.extractUInt(ME,0,17),-17);
+        double latLocal = Math.scalb(Bits.extractUInt(ME,17,17),-17);
+        IcaoAddress icaoAddress = rawMessage.icaoAddress();
+        int parity = Bits.extractUInt(ME,34,1);
+        long timeStampsNs = rawMessage.timeStampNs();
         return new AirbornePositionMessage(timeStampsNs,icaoAddress,altitudeFinale,parity,lonLocal,latLocal);
     }
 
