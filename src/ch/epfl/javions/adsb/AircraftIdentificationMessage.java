218fd2139e43a7e25aad79f11881676b6bdda43b
Sensiel <sennesiel2@gmail.com>
2023-03-21 15:06:20 +0100

    AircraftIdentificationMessage a moitié debug

--- src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
+++ src/ch/epfl/javions/adsb/AircraftIdentificationMessage.java
@@ -30,24 +30,17 @@ public record AircraftIdentificationMessage (long timeStampNs,
     public static AircraftIdentificationMessage of(RawMessage rawMessage){
         String string = "";
         for(int i = 7; i >= 0 ; i--){
-            int c = Bits.extractUInt(rawMessage.payload(),i*7,6) & 0xFF;
-            if(c == 0 || (c > 26 && c < 32 ) || (c > 32 && c < 48)) // methode prv ?
-                return null ;
-            if(c <= 26){
-                c += 64;
-            }
-            string += (char) c;
+            int c = Bits.extractUInt(rawMessage.payload(),i*6,6);
+            if((c >= '0' && c <= '9') || c == ' ' || (c >= 1 && c <= 26))
+                string += (char) c;
+            else return null;
         }
         CallSign callSign = new CallSign(string);
         IcaoAddress icaoAddress = rawMessage.icaoAddress();
-        int category = (14 - rawMessage.typeCode()) << 3 | Bits.extractUInt(rawMessage.payload(),48,3) ;
+        int category = ((14 - rawMessage.typeCode()) << 4) | Bits.extractUInt(rawMessage.payload(),48,3) ; // Potentiellement une erreur ici
         long timeStampsNs = rawMessage.timeStampNs();
 
         return new AircraftIdentificationMessage(timeStampsNs, icaoAddress, category, callSign);
     }
-
-
-
-
 }
 
