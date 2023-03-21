cc37c86461ee70db40737301879fccb893757eda
Sensiel <sennesiel2@gmail.com>
2023-03-21 14:17:32 +0100

    Cpr a moitie debug

--- src/ch/epfl/javions/adsb/CprDecoder.java
+++ src/ch/epfl/javions/adsb/CprDecoder.java
@@ -7,8 +7,8 @@ import ch.epfl.javions.Units;
 public class CprDecoder {
     private static final int ZLAT0 = 60;
     private static final int ZLAT1 = 59;
-    private static final int LARGEUR_0 = 1 / ZLAT0;
-    private static final int LARGEUR_1 = 1 / ZLAT1;
+    private static final double LARGEUR_0 = 1d / ZLAT0;
+    private static final double LARGEUR_1 = 1d / ZLAT1;
 
     private CprDecoder() {} //pour le rendre non instanciable
 
@@ -24,74 +24,55 @@ public class CprDecoder {
     public static GeoPos decodePosition(double x0, double y0, double x1, double y1, int mostRecent) {
         Preconditions.checkArgument(mostRecent == 1 || mostRecent == 0);
         //latitude
-        double resultLat;
-        double latPair, latImp;
+
         y0 = Math.scalb(y0,-17);
         y1 = Math.scalb(y1,-17);
-        double zoneLat = Math.rint((y0 * ZLAT1) - (y1 * ZLAT0));
-        if (zoneLat < 0) {
-                latPair = LARGEUR_0 * ((zoneLat + ZLAT0) + y0);
-                latImp = LARGEUR_1 * ((zoneLat + ZLAT1) + y1);
-        } else {
-                latPair = LARGEUR_0 * (zoneLat + y0);
-                latImp = LARGEUR_1 * (zoneLat + y1);
-        }
 
-        if(mostRecent == 0){
-            resultLat = latPair;
-        } else {
-            resultLat = latImp;
-        }
+        double zoneLat = Math.rint((y0 * ZLAT1) - (y1 * ZLAT0));
+        double latPair = (zoneLat < 0d) ? LARGEUR_0 * ((zoneLat + ZLAT0) + y0) : LARGEUR_0 * (zoneLat + y0);
+        double latImp = (zoneLat < 0d) ? LARGEUR_1 * ((zoneLat + ZLAT1) + y1) : LARGEUR_1 * (zoneLat + y1);
+
+        double resultLat;
+        resultLat = (mostRecent == 0) ? latPair : latImp;
 
         //creer methode prv pour check validité de lat
         double resultLatDegree = Units.convert(resultLat, Units.Angle.TURN, Units.Angle.DEGREE);
 
-        if (!(resultLatDegree >= -90 && resultLatDegree <= 90)) // jsp si 90 et -90 inclus ?
-            return null;
+        if (resultLatDegree < -90d || resultLatDegree > 90d) return null;
 
         // fin latitude
 
         // debut longitude
 
         // verification de l'égalité des zones
-        double aImp = Math.acos(1 - ((1 - Math.cos(2 * Math.PI * LARGEUR_0)) / Math.pow(Math.cos(latImp), 2)));
-        double aPair = Math.acos(1 - ((1 - Math.cos(2 * Math.PI * LARGEUR_0)) / Math.pow(Math.cos(latPair), 2)));
-        if(aPair != aImp){ return null; }
+        // Faut changer de TURN à Radian je pense
+        double aImp = Math.acos(1d - ((1d - Math.cos(2d * Math.PI * LARGEUR_0)) / Math.pow(Math.cos(latImp), 2d)));
+        double aPair = Math.acos(1d - ((1d - Math.cos(2d * Math.PI * LARGEUR_0)) / Math.pow(Math.cos(latPair), 2d)));
+        if(aPair != aImp) return null;
 
-        double resultLong;
         x0 = Math.scalb(x0,-17);
         x1 = Math.scalb(x1,-17);
-        double A = Math.acos(1 - ((1 - Math.cos(2 * Math.PI * LARGEUR_0)) / Math.pow(Math.cos(resultLat), 2)));
+
+        double A = Math.acos(1d - ((1d - Math.cos(2d * Math.PI * LARGEUR_0)) / Math.pow(Math.cos(resultLat), 2d)));
+        double resultLong;
         if (Double.isNaN(A)) {// normalement je dois utiliser isNaN()
-            if (mostRecent == 0) {
-                resultLong = x0;
-            } else {
-                resultLong = x1;
-            }
-        } else {
-            double zLong0 = Math.floor(2 * Math.PI / A);
-            double zLong1 = zLong0 - 1;
+            resultLong = (mostRecent == 0) ? x0 : x1;
+        }
+        else {
+            double zLong0 = Math.floor(2d * Math.PI / A);
+            double zLong1 = zLong0 - 1d;
             double zoneLong = Math.rint((x0 * zLong1) - (x1 * zLong0));
 
-            if (zoneLong < 0) {
-                if (mostRecent == 0) {
-                    resultLong = (1 / zLong0) * ((zoneLong + zLong0) + x0);
-                } else {
-                    resultLong = (1 / zLong1) * ((zoneLong + zLong1) + x1);
-                }
-            } else {
-                if (mostRecent == 0) {
-                    resultLong = (1 / zLong0) * (zoneLong + x0);
-                } else {
-                    resultLong = (1 / zLong1) * (zoneLong + x1);
-                }
-            }
+            if (zoneLong < 0)
+                resultLong = (mostRecent == 0) ? (1d / zLong0) * (zoneLong + zLong0 + x0) : (1d / zLong1) * (zoneLong + zLong1 + x1);
+            else
+                resultLong = (mostRecent == 0) ? (1d / zLong0) * (zoneLong + x0) : (1d / zLong1) * (zoneLong + x1);
         }
         // creer methode prv pour recentrer
-        if (resultLat >= 1 / 2 * Units.Angle.TURN) {
+        if (resultLat >= 1d / 2d * Units.Angle.TURN) {
             resultLat -= Units.Angle.TURN;
         }
-        if (resultLong >= 1 / 2 * Units.Angle.TURN) {
+        if (resultLong >= 1d / 2d * Units.Angle.TURN) {
             resultLong -= Units.Angle.TURN;
         }
 
